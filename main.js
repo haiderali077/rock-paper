f4d76bd693e2c3909957cd7d2fb70997ccc0dee2
haiderali077 <246067.haider@gmail.com.noreply.github.com>
2024-06-03 09:35:20 -0700

    basic function UI completed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
-let rounds = 0;
+const maxScore = 5;
 
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
@@ -17,26 +17,33 @@ function getComputerChoice() {
   }
 }
 
-let computerSelection = getComputerChoice();
-do {
-  const btn = document.querySelector("#btn-1");
-  btn.addEventListener("click", () => {
+const btn = document.querySelector("#btn-1");
+const btn2 = document.querySelector("#btn-2");
+const btn3 = document.querySelector("#btn-3");
+
+btn.addEventListener("click", () => {
+  if (computerScore < maxScore && humanScore < maxScore) {
+    let computerSelection = getComputerChoice();
     playRound("rock", computerSelection);
     checkResult();
-  });
+  }
+});
 
-  const btn2 = document.querySelector("#btn-2");
-  btn2.addEventListener("click", () => {
+btn2.addEventListener("click", () => {
+  if (computerScore < maxScore && humanScore < maxScore) {
+    let computerSelection = getComputerChoice();
     playRound("paper", computerSelection);
     checkResult();
-  });
+  }
+});
 
-  const btn3 = document.querySelector("#btn-3");
-  btn3.addEventListener("click", () => {
+btn3.addEventListener("click", () => {
+  if (computerScore < maxScore && humanScore < maxScore) {
+    let computerSelection = getComputerChoice();
     playRound("scissor", computerSelection);
     checkResult();
-  });
-} while (computerScore && humanScore < 5);
+  }
+});
 
 function playRound(string, computerChoice) {
   if (string === computerChoice) {
@@ -63,16 +70,22 @@ function playRound(string, computerChoice) {
 }
 
 function checkResult() {
-  if (humanScore && computerScore < 5) {
-    console.log("Computer score is " + computerScore);
-    console.log("Human score is " + humanScore);
-  } else {
+  const body = document.body;
+
+  body.append("Computer score is " + computerScore);
+  body.append(document.createElement("br"));
+  body.append("Human score is " + humanScore);
+  body.append(document.createElement("br"));
+  if (humanScore >= maxScore || computerScore >= maxScore) {
     if (computerScore > humanScore) {
-      console.log("Computer won! computer score is " + computerScore);
+      body.append("Computer won! Computer score is " + computerScore);
+      return;
     } else if (computerScore < humanScore) {
-      console.log("Human won! human score is " + humanScore);
+      body.append("Human won! Human score is " + humanScore);
+      return;
     } else {
-      console.log("It's a tie.");
+      body.append("It's a tie.");
+      return;
     }
   }
 }
