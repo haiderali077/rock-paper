9fe2d5931c664a4834b0603811a48bad521bc9e8
haiderali077 <113215081+haiderali077@users.noreply.github.com>
2024-06-23 01:34:17 -0700

    merge conflict fixed

--- main.js
+++ main.js
@@ -1,65 +1,24 @@
 let humanScore = 0;
 let computerScore = 0;
-<<<<<<< HEAD
-let rounds = 0;
-=======
 const maxScore = 5;
->>>>>>> rps-ui
 
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
   switch (num) {
     case 0:
       return "rock";
-      break;
     case 1:
       return "paper";
-      break;
     case 2:
       return "scissor";
-      break;
   }
 }
 
-<<<<<<< HEAD
-function getHumanChoice() {
-  let choice = window.prompt("Enter your choice: ");
-  return choice.toLowerCase();
-}
+const btnRock = document.querySelector("#btn-1");
+const btnPaper = document.querySelector("#btn-2");
+const btnScissor = document.querySelector("#btn-3");
 
-function playGame() {
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      return;
-    }
-    if (humanChoice === "rock" && computerChoice === "scissor") {
-      humanScore++;
-    }
-    if (humanChoice === "rock" && computerChoice === "paper") {
-      computerScore++;
-    }
-    if (humanChoice === "paper" && computerChoice === "rock") {
-      humanScore++;
-    }
-    if (humanChoice === "paper" && computerChoice === "scissor") {
-      computerScore++;
-    }
-    if (humanChoice === "scissor" && computerChoice === "rock") {
-      computerScore++;
-    }
-    if (humanChoice === "scissor" && computerChoice === "paper") {
-      humanScore++;
-=======
-const btn = document.querySelector("#btn-1");
-const btn2 = document.querySelector("#btn-2");
-const btn3 = document.querySelector("#btn-3");
-
-btn.addEventListener("click", () => {
+btnRock.addEventListener("click", () => {
   if (computerScore < maxScore && humanScore < maxScore) {
     let computerSelection = getComputerChoice();
     playRound("rock", computerSelection);
@@ -67,7 +26,7 @@ btn.addEventListener("click", () => {
   }
 });
 
-btn2.addEventListener("click", () => {
+btnPaper.addEventListener("click", () => {
   if (computerScore < maxScore && humanScore < maxScore) {
     let computerSelection = getComputerChoice();
     playRound("paper", computerSelection);
@@ -75,7 +34,7 @@ btn2.addEventListener("click", () => {
   }
 });
 
-btn3.addEventListener("click", () => {
+btnScissor.addEventListener("click", () => {
   if (computerScore < maxScore && humanScore < maxScore) {
     let computerSelection = getComputerChoice();
     playRound("scissor", computerSelection);
@@ -83,26 +42,21 @@ btn3.addEventListener("click", () => {
   }
 });
 
-function playRound(string, computerChoice) {
-  if (string === computerChoice) {
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
     return;
   }
-  if (string === "rock" && computerChoice === "scissor") {
+  if (humanChoice === "rock" && computerChoice === "scissor") {
     humanScore++;
-  }
-  if (string === "rock" && computerChoice === "paper") {
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
     computerScore++;
-  }
-  if (string === "paper" && computerChoice === "rock") {
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
     humanScore++;
-  }
-  if (string === "paper" && computerChoice === "scissor") {
+  } else if (humanChoice === "paper" && computerChoice === "scissor") {
     computerScore++;
-  }
-  if (string === "scissor" && computerChoice === "rock") {
+  } else if (humanChoice === "scissor" && computerChoice === "rock") {
     computerScore++;
-  }
-  if (string === "scissor" && computerChoice === "paper") {
+  } else if (humanChoice === "scissor" && computerChoice === "paper") {
     humanScore++;
   }
 }
@@ -117,37 +71,10 @@ function checkResult() {
   if (humanScore >= maxScore || computerScore >= maxScore) {
     if (computerScore > humanScore) {
       body.append("Computer won! Computer score is " + computerScore);
-      return;
     } else if (computerScore < humanScore) {
       body.append("Human won! Human score is " + humanScore);
-      return;
     } else {
       body.append("It's a tie.");
-      return;
->>>>>>> rps-ui
     }
   }
 }
-
-<<<<<<< HEAD
-while (rounds < 5) {
-  playGame();
-  rounds++;
-}
-
-if (computerScore > humanScore) {
-  console.log("Computer won! computer score is " + computerScore);
-}
-if (computerScore < humanScore) {
-  console.log("Human won! human score is " + humanScore);
-}
-if (computerScore == humanScore) {
-  console.log("Its a tie.");
-}
-=======
-//const resultDiv = document.querySelector(".choices");
-
-//const output = document.createElement("p");
-//output.textContent = results();
-//resultDiv.appendChild(output);
->>>>>>> rps-ui
